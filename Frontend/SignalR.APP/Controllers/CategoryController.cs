ebdd967ad4c766d912d03a7a1f3b048747fa2817
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-11-11 17:39:36 +0300

    Category list consume done

--- Frontend/SignalR.APP/Controllers/CategoryController.cs
+++ Frontend/SignalR.APP/Controllers/CategoryController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.APP.Models.Category;
 
 namespace SignalR.APP.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7191/api/Category/CategoryList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<CategoryViewModel>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
---
+++ Frontend/SignalR.APP/Models/Category/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SignalR.APP.Models.Category
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public bool Status { get; set; }
+    }
+}
