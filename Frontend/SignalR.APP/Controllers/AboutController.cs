e719e03e9233f2aea4de8e6d0528d0556962bf5a
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-11-26 13:56:36 +0300

    About page done for admin

---
+++ Frontend/SignalR.APP/Controllers/AboutController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.APP.Models.About;
+using SignalR.Dto.About;
+using System.Text;
+
+namespace SignalR.APP.Controllers
+{
+    public class AboutController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public AboutController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7191/api/About/AboutList");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<AboutViewModel>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        //[HttpGet]
+        //public IActionResult CreateAbout()
+        //{
+        //    return View();
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> CreateAbout(CreateAboutDto dto)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var jsonData = JsonConvert.SerializeObject(dto);
+        //    StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        //    var responseMessage = await client.PostAsync("https://localhost:7191/api/About/CreateAbout", content);
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+        //public async Task<IActionResult> DeleteAbout(int id)
+        //{
+        //    var client = _httpClientFactory.CreateClient();
+        //    var responseMessage = await client.DeleteAsync($"https://localhost:7191/api/About/DeleteAbout?id={id}");
+        //    if (responseMessage.IsSuccessStatusCode)
+        //    {
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+        [HttpGet]
+        public async Task<IActionResult> UpdateAbout(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7191/api/About/GetAbout?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateAboutDto>(jsonData);
+                return View(values);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateAbout(UpdateAboutDto dto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(dto);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7191/api/About/UpdateAbout", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
