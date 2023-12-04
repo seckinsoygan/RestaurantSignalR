8eeeea85ae37eb7694d0544ba57f970d79c20561
Niyazi Seçkin Soygan <seckinsoygann@gmail.com>
2023-12-04 22:28:56 +0300

    SignalR added

--- Backend/SignalR.API/Program.cs
+++ Backend/SignalR.API/Program.cs
@@ -1,9 +1,19 @@
+using SignalR.API.Hubs;
 using SignalR.API.Mapping;
 using SignalR.BLL.DependencyResolvers;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyHeader().AllowAnyMethod().SetIsOriginAllowed((host) => true).AllowCredentials();
+    });
+});
+builder.Services.AddSignalR();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,11 +31,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<SignalRHub>("/signalrhub");
 
 app.Run();
---
+++ Frontend/SignalR.APP/Controllers/SignalRDefault.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR.APP.Controllers
+{
+    public class SignalRDefault : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
